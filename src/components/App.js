7afb2dca6a2e206c4dc0866b0a44e9118a932484
Cody <codybarker.or@gmail.com>
2023-03-15 10:48:09 -0700

    map with borders. area names need cleaning up

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Segment } from "semantic-ui-react";
 import "../stylesheets/App.css";
+import WestWorldMap from './WestworldMap'
+import Headquarters from './Headquarters'
 
 function App() {
+
+  const [hosts, setHosts] = useState([])
+  const [areas, setAreas] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/hosts')
+    .then(r => r.json())
+    .then(hosts => setHosts(hosts))
+  },[])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/areas')
+    .then(r => r.json())
+    .then(areas => setAreas(areas))
+  }, [])
+
   return (
     <Segment id="app">
-      {/* What components should go here? Check out Checkpoint 1 of the Readme if you're confused */}
+      <WestWorldMap hosts={hosts} areas={areas}/>
+      <Headquarters hosts={hosts} areas={areas}/>
     </Segment>
   );
 }
 
 export default App;
+
+/**App
+ *  WestWorldMap
+ *    Area
+ *      Host
+ *  Headquarters
+ *    ColdStorage
+ *      HostList
+ *        Host
+ *    Details
+ *      Host Info
+ *    Log Panel
+ *    
+*/
+      
--- src/components/WestworldMap.js
+++ src/components/WestworldMap.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Segment } from "semantic-ui-react";
+import Area from './Area'
 
-function WestworldMap() {
-  return <Segment id="map">{/* What should we render on the map? */}</Segment>;
+function WestworldMap({hosts, areas}) {
+  const areaComps = areas.map(area => <Area key={area.id} area={area}/>)
+  return <Segment id="map">{areaComps}</Segment>;
 }
 
 export default WestworldMap;
